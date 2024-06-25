6af1ef186bc98dc19b0890e08cca9328fbc4f0c9
João Targino <joaoetargino@gmail.com>
2024-06-25 20:17:51 -0300

    chore: added validation and encryption modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,6 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import AppDataSource from './config/data-source';
 
 @Module({
   imports: [
