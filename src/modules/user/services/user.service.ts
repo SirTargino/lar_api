ffbb4c0ef043e59d9f4175266f221dfaa9300f6d
João Targino <joaoetargino@gmail.com>
2024-06-25 20:14:53 -0300

    feat: created user service and user service test

---
+++ src/modules/user/services/user.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { User } from "../user.entity";
+import { Repository } from "typeorm";
+import { CreateUserDto } from "../dto/create-user.dto";
+import * as bcrypt from "bcrypt";
+
+@Injectable()
+export class UserService {
+    constructor(
+        @InjectRepository(User) private readonly userRepository: Repository<User>
+    ) {}
+
+    async createUser(createUserDto: CreateUserDto){
+        const newUser: User = this.userRepository.create(createUserDto);
+
+        newUser.password = await bcrypt.hash(createUserDto.password, 10);
+
+        return await this.userRepository.save({...newUser})
+    }
+
+    async getAllUsers(){
+        const allUsers = this.userRepository.find()
+        return allUsers;
+    }
+}
