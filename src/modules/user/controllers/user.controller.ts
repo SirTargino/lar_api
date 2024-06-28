0dd50c41bd05b29b19fa0131ece186fec02ea30b
João Targino <joaoetargino@gmail.com>
2024-06-28 17:25:42 -0300

    feat: created GET by name route, PUT route and DELETE route

--- src/modules/user/controllers/user.controller.ts
+++ src/modules/user/controllers/user.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Get, Post, UseGuards } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Post, Put, Query, UseGuards } from "@nestjs/common";
 import { UserService } from "../services/user.service";
 import { CreateUserDto } from "../dto/create-user.dto";
 import { AuthGuard } from "@nestjs/passport";
+import { UpdateUserDto } from "../dto/update-user.dto";
 
 @Controller('user')
 export class UserController{
@@ -19,4 +20,22 @@ export class UserController{
     findAll(){
         return this.userService.getAllUsers();
     };
+
+    @Get('search')
+    findByName(@Query('name') name: string){
+        return this.userService.getUserByName(name);
+    }
+
+    @Put(':id')
+    edit(
+        @Param('id') id: string,
+        @Body() updateUserDto: UpdateUserDto
+    ){
+        return this.userService.editUser(id, updateUserDto);
+    }
+
+    @Delete(':id')
+    deleteUser(@Param('id') id: string){
+        return this.userService.deleteUser(id);
+    }
 }
